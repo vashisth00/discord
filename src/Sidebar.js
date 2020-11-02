c1c3ee895f6cb008b5aa6cb54fcee85ec678885b
Vashisth00 <vashisthbhushan@gmail.com>
2020-11-03 01:31:59 +0530

    More UI fix

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,6 +3,13 @@ import './Sidebar.css';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
 import SidebarChannel from './SidebarChannel';
+import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt'
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined'
+import CallIcon from '@material-ui/icons/Call'
+import { Avatar } from '@material-ui/core'
+import MicIcon from '@material-ui/icons/Mic'
+import HeadsetIcon from '@material-ui/icons/Headset'
+import SettingsIcon from '@material-ui/icons/Settings'
 
 function Sidebar() {
     return (
@@ -21,8 +28,36 @@ function Sidebar() {
                     <AddIcon className="sidebar__addChannel"></AddIcon>
                 </div>
                 <div className="sidebar__channelsList">
-                    <SidebarChannel></SidebarChannel>
+                    <SidebarChannel/>
+                    <SidebarChannel/>
+                    <SidebarChannel/>
+                    <SidebarChannel/>
                 </div>
+                <div className="sidebar__voice">
+                <SignalCellularAltIcon className='sidebar__voiceIcons' fontSize='large' />
+                <div className="sidebar__voiceInfo">
+                    <h3>Voice Connected</h3>
+                    <p>Stream</p>
+                </div>
+
+                <div className="sidebar__voiceIcons">
+                    <InfoOutlinedIcon />
+                    <CallIcon />
+                </div>
+            </div>
+            <div className="sidebar__profile">
+                <Avatar src="#" onClick={() => null()} /> 
+                <div className="sidebar__profileInfo">
+                    <h3>VB</h3>
+                    <p>bb</p>
+                </div>
+
+                <div className="sidebar__profileIcons">
+                    <MicIcon />
+                    <HeadsetIcon />
+                    <SettingsIcon />
+                </div>
+            </div>
             </div>
         </div>
     )
