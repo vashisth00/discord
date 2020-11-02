6cc80ae78ec6751dd38c132b1a2d5772524592ae
Vashisth00 <vashisthbhushan@gmail.com>
2020-11-02 17:02:05 +0530

    Adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
+    //BEM naming convention
     <div className="App">
     <h1>Hi</h1>
+    <Sidebar></Sidebar>
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            hii
+        </div>
+    )
+}
+
+export default Sidebar
